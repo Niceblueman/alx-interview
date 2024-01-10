c246d42214ec7774a294ffca0c014fdcf898694e
Niceblueman <wintheiser761@gmail.com>
2024-01-10 03:12:28 +0100

    - add #!/usr/bin/python3

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
     if n <= 0:
         return []
-
     triangle = [[1]]
-
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
