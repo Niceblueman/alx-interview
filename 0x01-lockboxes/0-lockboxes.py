e04e9eba44ecf1196615cd195f8c547f6b37d52a
Niceblueman <wintheiser761@gmail.com>
2024-01-15 22:04:52 +0100

    - fix test 9

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
 """
-Solves the lock boxes puzzle with a more detailed implementation.
+    lock boxes puzzle
 """
 
 
 def canUnlockAll(boxes):
-    """
-    Check if all boxes can be opened.
-
+    """Check if all boxes can be opened
     Args:
-        boxes (list): List containing all the boxes with the keys.
-
+        boxes (list): List which contains all the boxes with the keys
     Returns:
-        bool: True if all boxes can be opened, otherwise, False.
+        bool: True if all boxes can be opened, otherwise, False
     """
-    if not boxes or not boxes[0]:
-        return False
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
 
-    num_boxes = len(boxes)
-    unlocked_boxes = set([0])
-    keys_to_check = boxes[0]
+    opened_boxes = {0: {'status': 'opened', 'keys': boxes[0]}}
 
-    while keys_to_check:
-        key = keys_to_check.pop(0)
+    while True:
+        keys = None
+        for index, box in opened_boxes.items():
+            if box.get('status') == 'opened':
+                box['status'] = 'opened/checked'
+                keys = box.get('keys')
+                break
 
-        if key < num_boxes and key not in unlocked_boxes:
-            unlocked_boxes.add(key)
-            keys_to_check.extend(boxes[key])
+        if keys:
+            for key in keys:
+                try:
+                    if opened_boxes.get(key) and opened_boxes.get(key).get('status') == 'opened/checked':
+                        continue
+                    opened_boxes[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in opened_boxes.values()]:
+            continue
+        elif len(opened_boxes) == len(boxes):
+            break
+        else:
+            return False
 
-    return len(unlocked_boxes) == num_boxes
+    return len(opened_boxes) == len(boxes)
