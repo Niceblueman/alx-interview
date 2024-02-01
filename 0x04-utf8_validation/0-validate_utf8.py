c8988a7fcf179d04e974b9b168f56df5540a8fb9
Niceblueman <wintheiser761@gmail.com>
2024-02-02 00:34:32 +0100

    - fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,34 +2,57 @@
 """utf-8 validation function"""
 
 
-def validUTF8(data):
+def validUTF8(bytes):
     """validate supposed utf8 data 
 
     Args:
-        data (list[int]): _description_
+        bytes (list[int]): _description_
 
     Returns:
         bool: if Valid utf8
     """
-    size = 0
-    for char in data:
-        if size == 0 and char >> 7 == 0b0:
-            continue
-        elif size == 0:
-            if char >> 5 == 0b110:
-                size = 1
-            elif char >> 4 == 0b1110:
-                size = 2
-            elif char >> 3 == 0b11110:
-                size = 3
-            elif char >> 2 == 0b111110:
-                size = 4
-            elif char >> 1 == 0b1111110:
-                size = 5
+    def char_length(bytes):
+        """_summary_
+
+        Args:
+            bytes (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        char_count = 0
+        i = 0
+
+        while i < len(bytes):
+            char_count += 1
+
+            # Lead byte analysis
+            if bytes[i] & 0b10000000 == 0b00000000:
+                i += 1
+                continue
+            elif bytes[i] & 0b11100000 == 0b11000000:
+                expected_len = 2
+            elif bytes[i] & 0b11110000 == 0b11100000:
+                expected_len = 3
+            elif bytes[i] & 0b11111000 == 0b11110000:
+                expected_len = 4
+            elif bytes[i] & 0b11111100 == 0b11111000:
+                expected_len = 5
+            elif bytes[i] & 0b11111110 == 0b11111100:
+                expected_len = 6
             else:
-                return False
-        else:
-            if (char >> 6) != 0b10:
-                return False
-            size -= 1
-    return size == 0
+                return -1
+
+            # Count trailing bytes
+            while expected_len > 1:
+                i += 1
+                if i >= len(bytes):
+                    return -1
+                if bytes[i] & 0b11000000 != 0b10000000:
+                    return -1
+                expected_len -= 1
+
+            i += 1
+
+        return char_count
+    return char_length(bytes) != -1
