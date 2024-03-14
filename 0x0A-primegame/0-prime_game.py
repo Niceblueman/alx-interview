36ad7ffec2a271019f53a73986573d0b6e59ec4c
Niceblueman <wintheiser761@gmail.com>
2024-03-14 21:45:37 +0000

    - all

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Prime Game - Maria and Ben are playing a game"""
+
+
 def is_prime(num):
     if num <= 1:
         return False
@@ -7,17 +10,20 @@ def is_prime(num):
             return False
     return True
 
+
 def get_primes(n):
+
     primes = []
     for i in range(2, n + 1):
         if is_prime(i):
             primes.append(i)
     return primes
 
+
 def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         primes = get_primes(n)
         total_primes = len(primes)
@@ -25,7 +31,7 @@ def isWinner(x, nums):
             ben_wins += 1
         else:
             maria_wins += 1
-    
+
     if maria_wins == ben_wins:
         return None
     elif maria_wins > ben_wins:
