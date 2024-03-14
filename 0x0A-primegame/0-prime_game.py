b3ecc74aa04babd3d5571d1d1812fc83ac0f4a54
Niceblueman <wintheiser761@gmail.com>
2024-03-14 21:54:38 +0000

    - """Prime Game - Maria and Ben are playing a game"""

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,6 +6,14 @@ def isWinner(x, nums):
     """x - rounds
     nums - numbers list
     """
+    def remove_prime_multiples(ls, x):
+        """removes multiples of primes"""
+        for i in range(2, len(ls)):
+            try:
+                ls[i * x] = 0
+            except (ValueError, IndexError):
+                break
+
     if x <= 0 or nums is None:
         return None
     if x != len(nums):
@@ -14,10 +22,9 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a = [1 for _ in range(sorted(nums)[-1] + 1)]
     a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
+    [remove_prime_multiples(a, i) for i in range(2, len(a))]
 
     for i in nums:
         if sum(a[0:i + 1]) % 2 == 0:
@@ -29,14 +36,3 @@ def isWinner(x, nums):
     if maria > ben:
         return "Maria"
     return None
-
-
-def rm_multiples(ls, x):
-    """removes multiple
-    of primes
-    """
-    for i in range(2, len(ls)):
-        try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
-            break
